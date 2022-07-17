bfbc77648edf6ac3b127ba5d0ec8afbd6836927e
sbx_user1051 <sbx_user1051@169.254.64.133>
2022-07-17 22:59:09 +0000

    Sun Jul 17 22:59:09 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -14,4 +14,7 @@ def checkout(skus: str) -> int:
 
 
 def split_sku_str_number(skus: str) -> List[Union[str, int]]:
+    r = re.compile(r"(\d+[A-Z]{1})|([A-Z])")
+    result = r.match(skus).groups()
     return [i for i in re.split(r"(\d+[A-Z]{1})|([A-Z])", "XYZ3BAT") if i]
+
