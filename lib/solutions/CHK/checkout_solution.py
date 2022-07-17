07385f7f10d3981d2a7d13cca661220789f4dd36
sbx_user1051 <sbx_user1051@169.254.64.133>
2022-07-17 22:23:09 +0000

    Sun Jul 17 22:23:09 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,9 +11,10 @@ def checkout(skus: str) -> int:
 
 
 def split_sku_str_number(skus: str) -> List:
-    match = re.match(r"([a-z]+)([0-9]+)", skus, re.I)
+    match = re.split("(/d)", skus)
     if match:
         items = match.groups()
     print(items)
+
 
 
