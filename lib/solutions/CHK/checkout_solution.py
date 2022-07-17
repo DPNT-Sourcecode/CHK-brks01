2915c791127928b79530e633c071fec91f11c7d8
sbx_user1051 <sbx_user1051@169.254.64.133>
2022-07-17 22:20:09 +0000

    Sun Jul 17 22:20:09 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,6 +2,18 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+from typing import List
+import re
+
+
 def checkout(skus: str) -> int:
     return 0
+
+
+def split_sku_str_number(skus: str) -> List:
+    match = re.match(r"([a-z]+)([0-9]+)", skus, re.I)
+    if match:
+        items = match.groups()
+    print(items)
+
 
--- test/solution_tests/CHK_R1/test_checkout_r1.py
+++ test/solution_tests/CHK_R1/test_checkout_r1.py
@@ -9,3 +9,8 @@ class TestCheckout:
     def test_checkout_round_1_fail(self) -> None:
         result = checkout_solution.checkout("2A3C")
         assert result == -1
+
+    def test_split_str_number(self) -> None:
+        result = checkout_solution.split_sku_str_number("3AB2C")
+        print(result)
+
