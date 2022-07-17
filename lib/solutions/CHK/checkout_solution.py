138d48f01b4707f5a6ceb451ec2643d8d82fe060
sbx_user1051 <sbx_user1051@169.254.64.133>
2022-07-17 23:47:39 +0000

    Sun Jul 17 23:47:39 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -27,6 +27,8 @@ def checkout(skus: str) -> int:
     total = 0
     for item in container:
         tmp = get_special_price(item[0])
+        if tmp[2] == 0:
+            continue
         if tmp[0] and item[1] >= tmp[0]:
             discount = item[1] // tmp[0]
             non_discount = item[1] % tmp[0]
@@ -45,6 +47,7 @@ def split_sku_str_number(skus: str) -> List[Tuple[str, int]]:
         else:
             modified_results.append((item[-1], int(item[:-1])))
     return modified_results
+
 
 
 
--- test/solution_tests/CHK_R1/test_checkout_r1.py
+++ test/solution_tests/CHK_R1/test_checkout_r1.py
@@ -4,7 +4,7 @@ from lib.solutions.CHK import checkout_solution
 class TestCheckout:
     def test_checkout_round_1(self) -> None:
         result = checkout_solution.checkout("AxA")
-        assert result == 160
+        assert result == 100
 
     def test_checkout_round_1_fail(self) -> None:
         result = checkout_solution.checkout(123)
@@ -17,6 +17,7 @@ class TestCheckout:
     def test_symbols_input(self) -> None:
         result = checkout_solution.checkout("-")
         assert result == -1
+
 
 
 
