bc5989f156d7643591fc2c80c609e088cfc33954
sbx_user1051 <sbx_user1051@169.254.224.69>
2022-07-18 04:49:57 +0000

    Mon Jul 18 04:49:57 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -4,7 +4,7 @@ from lib.solutions.CHK import checkout_solution
 
 
 class TestCheckout:
-    @pytest.mark.parametrize("inputs", [("4A2B2E", 275), ("AAA", 130), ("", 0), ("3A2BC2A", 265)])
+    @pytest.mark.parametrize("inputs", [("A", 40), ("4A2B2E", 275), ("AAA", 130), ("", 0), ("3A2BC2A", 265)])
     def test_checkout_round_1(self, inputs: Tuple[Any, int]) -> None:
         assert checkout_solution.checkout(inputs[0]) == inputs[1]
 
@@ -19,3 +19,4 @@ class TestCheckout:
 
     def test_get_offers(self) -> None:
         assert checkout_solution.get_offers("C") == []
+
