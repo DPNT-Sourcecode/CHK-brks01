1f0401e844cafdbe09075aaf567dba71a07b833d
sbx_user1051 <sbx_user1051@169.254.64.133>
2022-07-18 00:37:05 +0000

    Mon Jul 18 00:37:05 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -46,7 +46,6 @@ def split_sku_str_number(skus: str) -> List[Tuple[str, int]]:
     for item in results:
         if len(item) == 1 and item not in tmp:
             tmp[item] = 1
-            # modified_results.append((item, 1))
         elif len(item) > 1:
             if item[-1] not in tmp:
                 tmp[item[-1]] = int(item[:-1])
@@ -55,6 +54,7 @@ def split_sku_str_number(skus: str) -> List[Tuple[str, int]]:
         else:
             tmp[item] += 1
     return list(zip(tmp.keys(), tmp.values()))
+
 
 
 
